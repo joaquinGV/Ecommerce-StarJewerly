d564edad276911031c3faf0b1f869ac12cf4a7fc
Joaquin Gonzalez <joaquinagv99@hotmail.com>
2023-07-30 03:51:33 -0600

    rutas privadas

--- React-StarJewerlyProject/src/routes/AppRoutes.jsx
+++ React-StarJewerlyProject/src/routes/AppRoutes.jsx
@@ -6,10 +6,16 @@ import { routes } from "./routes";
 const AppRouter = () => {
   return (
     <Routes>
+      {/* Rutas publicas */}
       <Route element={<Layout />}>
         {routes.map(({ id, path, Element }) => (
           <Route key={id} path={path} element={<Element />} />
         ))}
+      </Route>
+
+      {/* Rutas privadas */}
+      <Route element={<ProtectedRoutes/>}>
+        <Route></Route>
       </Route>
 
       <Route path="*" element={<h1>404 not found</h1>} />
